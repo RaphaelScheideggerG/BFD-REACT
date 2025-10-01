c925d523e26ba4ea36b8e25f844b954db56f0116
RaphaelScheideggerG <raphaelscheideggerguedes0406@gmail.com>
2025-10-01 06:18:05 -0300

    app02

--- App02/src/App.jsx
+++ App02/src/App.jsx
@@ -1,9 +1,10 @@
-import Login from './componentes/Login.jsx';
-
+// (1) Componente funcional padrão do React
 export default function App() {
+  // (2) Retorna a interface: este conteúdo aparecerá dentro do #root
   return (
-    <>
-      <Login />
-    </>
+    <main style={{ padding: 16 }}>
+      <h1>ReactJS — Bolsa Futuro Digital</h1>
+      <p>Bem-vindos! Este é o esqueleto inicial de uma SPA com Vite.</p>
+    </main>
   );
 }
