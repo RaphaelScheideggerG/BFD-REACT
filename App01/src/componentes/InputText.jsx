470927094727be5839f9441f7996b1779d851f9a
RaphaelScheideggerG <git@stackblitz.com>
2025-10-01 00:35:33 -0300

    Initial commit

---
+++ App01/src/componentes/InputText.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function InputText(props) {
+  return <input type="text" placeholder={props.placeholder} />;
+}
---
+++ App01/src/componentes/Login.jsx
@@ -0,0 +1,20 @@
+import Labelln from './Labelln.jsx';
+import Label from './label.jsx';
+import Button from './Button.jsx';
+import InputText from './InputText.jsx';
+
+function Login() {
+  return (
+    <>
+      <Labelln texto="Login" />
+      <Label texto="Usuário: " />
+      <InputText placeholder="user..." /> <br />
+      <Label texto="Senha: " />
+      <InputText placeholder="password..." /> <br />
+      <Button valor="Logar" />
+      <Button valor="Cadastrar" />
+    </>
+  );
+}
+
+export default Login;
